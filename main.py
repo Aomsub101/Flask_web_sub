95ab67d7f440dfca7227d6009645332144e59fe7
Aomsub <lerdariyamaythee@gmail.com>
2025-01-28 11:43:35 +0700

    Add files + modified

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from markupsafe import escape
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/hello")
+@app.route("/hello/<name>")
+def greeting(name=None):
+    return render_template('greeting.html', name=name)
 
 @app.route("/test")
 def testing():
@@ -14,4 +20,4 @@ def testing():
 @app.route("/sum/<int:num1>/<int:num2>")
 def get_sum(num1, num2):
     result = num1**2 + num2**2
-    return f"<p>The sum of {num1}^2 + {num2}^2 = {result}</p>"
+    return render_template('calculate.html', result=result)
